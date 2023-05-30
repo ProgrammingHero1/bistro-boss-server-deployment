1b570bc2e913962a090c5274ed471b4867b51f7e
Programming Hero <programming.hero1@gmail.com>
2023-05-30 08:45:26 -0500

    delete menu item implemented for admin

--- index.js
+++ index.js
@@ -136,6 +136,13 @@ async function run() {
       res.send(result);
     })
 
+    app.delete('/menu/:id', verifyJWT, verifyAdmin, async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) }
+      const result = await menuCollection.deleteOne(query);
+      res.send(result);
+    })
+
     // review related apis
     app.get('/reviews', async (req, res) => {
       const result = await reviewCollection.find().toArray();
