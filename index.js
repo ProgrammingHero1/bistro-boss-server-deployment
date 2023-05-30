14c86aa66d862a61b54abf6d1ea241fdb3aab7e2
Programming Hero <programming.hero1@gmail.com>
2023-05-30 07:52:13 -0500

    save new item and verify user and admin

--- index.js
+++ index.js
@@ -130,7 +130,7 @@ async function run() {
       res.send(result);
     })
 
-    app.post('/menu', async (req, res) => {
+    app.post('/menu', verifyJWT, verifyAdmin, async (req, res) => {
       const newItem = req.body;
       const result = await menuCollection.insertOne(newItem)
       res.send(result);
