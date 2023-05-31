0252af8315ce46c8d85a5c85ed0cb9107e666480
Programming Hero <programming.hero1@gmail.com>
2023-05-31 08:19:12 -0500

    verify token for payment intent

--- index.js
+++ index.js
@@ -183,7 +183,7 @@ async function run() {
     })
 
     // create payment intent
-    app.post('/create-payment-intent', async (req, res) => {
+    app.post('/create-payment-intent', verifyJWT, async (req, res) => {
       const { price } = req.body;
       const amount = price * 100;
       const paymentIntent = await stripe.paymentIntents.create({
