3604fe614063bb7c78118e0e7fed64f64b135772
Programming Hero <programming.hero1@gmail.com>
2023-05-31 16:05:24 -0500

    save payment info to the database

--- index.js
+++ index.js
@@ -49,6 +49,7 @@ async function run() {
     const menuCollection = client.db("bistroDb").collection("menu");
     const reviewCollection = client.db("bistroDb").collection("reviews");
     const cartCollection = client.db("bistroDb").collection("carts");
+    const paymentCollection = client.db("bistroDb").collection("payments");
 
     app.post('/jwt', (req, res) => {
       const user = req.body;
@@ -195,6 +196,18 @@ async function run() {
       res.send({
         clientSecret: paymentIntent.client_secret
       })
+    })
+
+
+    // payment related api
+    app.post('/payments', verifyJWT, async(req, res) =>{
+      const payment = req.body;
+      const insertResult = await paymentCollection.insertOne(payment);
+
+      const query = {_id: { $in: payment.cartItems.map(id => new ObjectId(id)) }}
+      const deleteResult = await cartCollection.deleteMany(query)
+
+      res.send({ insertResult, deleteResult});
     })
 
 
