5934d1508ad0042a7583b0def2b5314dbc01f1d6
Programming Hero <programming.hero1@gmail.com>
2023-06-02 20:13:12 -0500

    handle non integer error for stripe

--- index.js
+++ index.js
@@ -186,7 +186,7 @@ async function run() {
     // create payment intent
     app.post('/create-payment-intent', verifyJWT, async (req, res) => {
       const { price } = req.body;
-      const amount = price * 100;
+      const amount = parseInt(price * 100);
       const paymentIntent = await stripe.paymentIntents.create({
         amount: amount,
         currency: 'usd',
